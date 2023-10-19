b3d5e0b6f4e88b2334e6814621dddcf4181d5e66
lorelof <138538721+lorelof@users.noreply.github.com>
2023-10-18 22:54:21 -0300

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -1,23 +1,43 @@
-import {traerProductos} from "./main.js";
-import { crearCards } from "./main.js";
+import {bajarProducto} from "./main.js";
+// export {imprimirCart};
 
-let cart = [];
-const bigContainer = document.querySelector('.bigContainerCart');
-const cardCart=document.getElementById('containerCart');
-let total=document.createElement('h3');
-let subtotal=0;
+// let cart = [];
+// const bigContainer = document.querySelector('.bigContainerCart');
+// const cardCart=document.getElementById('containerCart');
+// let total=document.createElement('h3');
+// let subtotal=0;
 
-traerProductos('js/data.json')
-    .then((products)=>crearCards(products, cardCart))
 
-async function traerProductos(){
-    const resp = await fetch('js/data.json');
-    const data = await resp.json();
-    crearCards(data);
+let prod=bajarProducto("carrito")
+imprimirCart(prod);
+
+const imprimirCart = function (products){
+    console.log(products);
+    alert("printCart")
+    const containerCart=document.querySelector('.containerCart');
+    const cardCart = document.createElement("article")
+    const nombre = document.createElement("h3")
+    const imagen = document.createElement("img")
+    const precio = document.createElement("h4")
+    const description = document.createElement("h6");
+    const btnFinish=document.createElement('button')
+    nombre.innerHTML = products.title
+    imagen.src = products.image
+    precio.innerHTML = products.price
+    description.innerHTML= products.description;
+    btnFinish.innerHTML="Finalizar Compra";
+    // containerCart.innerHTML+=`
+    // <article class="cart" id="cart"> 
+    // <h3 class="title">${products[i].title}</h3>
+    // <img src="${products[i].image}" alt="${products[i].title}">
+    // <h4 class="price">${products[i].price}</h4>
+    // <button class="btnFinish" id="btnFinish-${products[i].id}" >Finalizar compra</button>
+    // </article>
+    // `    
+
+    containerCart.append(cardCart);
 }
-traerProductos();
 
-//seguir con el cart.js vinc con el cart.html
 
 
 
--- js/detail.js
+++ js/detail.js
@@ -1,31 +1,6 @@
-import {imprimirDetalle} from "./main.js";
-import {crearCards} from "./main.js";
-// export {mostrarDetail};
-
-let prod=[];
-let prodDetallado={};
-imprimirDetalle(prodDetallado);
+import {imprimirDetalle, bajarProducto} from "./main.js";
 
 
+let prod=bajarProducto("carrito")
+imprimirDetalle(prod);
 
-
-const containerDetail = document.querySelector("#containerDetail");
-let total = document.createElement("h4");
-let subt = 0;
-
-// traerProductos('js/data.json')
-//     .then((products)=>crearCards(products, containerDetail))
-
-
-//     const mostrarDetail = function (id, products){
-//         alert("md")
-//         // if(id==products.id){
-
-//         //error en products.filter
-//             prodDetallados=products.filter((products)=>{
-//                 id==prodDetallados;
-//             })
-//             console.log(prodDetallados);
-        
-//     alert("h")
-// }
